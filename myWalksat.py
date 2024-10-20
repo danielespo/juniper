c452f9e920be187631761e38849da3dfa36c764f
Daniel Alberto Espinosa Gonzalez <dae@asimov.ece.ucsb.edu>
2024-10-20 15:23:40 -0700

    adding wasted cycles

--- myWalksat.py
+++ myWalksat.py
@@ -12,27 +12,36 @@ from multiprocessing import Pool
 def get_stats(walksat_result, max_flips):
     # walksat_result = [[assignment, _try, _loop, flips], ...]
     flips_list = []
+    loops_list = []
+    tries_list = []
     success_flags = []
-
+    max_tries = 10 #change me
     for res in walksat_result:
         if res != "FAIL":
             # if not a fail then a success
-            assignment, _Tries, _Flips, flips = res
+            assignment, _Tries, _Loops, flips = res
             # Flips here has to be flips and not _flips because
             # _flips is actually _loops in wsatA1 and does not correspond
             # to actual flips per the algorithm. In walksat.py the flips
             # variable is duplicated for this reason.
+            # Renamed to "_Loops" to remove confusion
             flips_list.append(flips)
+            loops_list.append(_Loops)
+            tries_list.append(_Tries)
             success_flags.append(1)
         else:
             # if fail then fail
             flips_list.append(max_flips)
+            loops_list.append(max_flips)
+            tries_list.append(max_tries)
             success_flags.append(0)
 
     flips_array = np.array(flips_list)
+    loops_array = np.array(loops_list)
+    tries_array = np.array(tries_list)
     success_array = np.array(success_flags)
 
-    walksat_result_arr = np.column_stack((flips_array, success_array))
+    walksat_result_arr = np.column_stack((flips_array, loops_list, tries_list, success_array))
 
     prob_s = np.mean(success_array)
     success_ind = np.where(walksat_result_arr[:, 1] == 1)[0]
@@ -41,9 +50,20 @@ def get_stats(walksat_result, max_flips):
     if len(success_ind) > 0:
         avg_flips = np.mean(walksat_result_arr[success_ind, 0])
         std_flips = np.std(walksat_result_arr[success_ind, 0])
+
+        avg_loops = np.mean(walksat_result_arr[success_ind, 1])
+        std_loops = np.std(walksat_result_arr[success_ind, 1])
+
+        avg_tries = np.mean(walksat_result_arr[success_ind, 2])
+        std_tries = np.std(walksat_result_arr[success_ind, 2])
     else:
         avg_flips = 0
         std_flips = 0
+        avg_loops = 0
+        std_loops = 0
+        avg_tries = 0
+        std_tries = 0
+
 
     max_restarts = walksat_result_arr.shape[0]
     # Same as tinish code
@@ -61,7 +81,7 @@ def get_stats(walksat_result, max_flips):
         else:
             tts_99 = float('inf')  # If no success then no TTS99
 
-    return avg_flips, prob_s, std_flips, tts_99
+    return avg_flips, prob_s, std_flips, tts_99, avg_loops, std_loops, avg_tries, std_tries
 
 
 def API(filepath, max_tries, max_flips, probability, mode):
@@ -97,6 +117,10 @@ def API(filepath, max_tries, max_flips, probability, mode):
     with Pool(processes=10) as pool:
         result_objects = []
 
+        # The range(10) means 10 independent tries of max_tries and max_flips each
+        # To get the CDF plot, must increase max_flips slowly and then get the data from there
+        # Adding a mode called "CDF" to do this, in a second.
+
         if mode == "walksat":
             for _ in range(10):
                 res = pool.apply_async(wk.walkSAT, args=(clauses, max_tries, max_flips, probability))
@@ -106,7 +130,7 @@ def API(filepath, max_tries, max_flips, probability, mode):
             colors = a1.GenerateColors(clauses)
             max_loops = math.floor(max_flips / len(colors))
             for _ in range(10):
-                res = pool.apply_async(a1.AlgorithmA1, args=(clauses, colors, max_tries, max_loops, probability, 0))
+                res = pool.apply_async(a1.AlgorithmA1, args=(clauses, colors, max_tries, max_loops, probability, 0)) # assignment[1:], _try, _loop, flips
                 result_objects.append(res)
 
         elif mode == "coloringA1_heuristic1":
@@ -135,7 +159,7 @@ def API(filepath, max_tries, max_flips, probability, mode):
     pool.close()
     pool.join()
     
-    avg_flips, prob_s, std_flips, tts_99 = get_stats(walksat_result, max_flips)
+    avg_flips, prob_s, std_flips, tts_99, avg_loops, std_loops, avg_tries, std_tries = get_stats(walksat_result, max_flips)
 
     # NOTE: Return also the avg_iterations , the specific number of iterations (loops)
     # And then replot.
@@ -147,7 +171,7 @@ def API(filepath, max_tries, max_flips, probability, mode):
     # function of the probability (i.e increment the number of flips)
     # easiest way to do that: make a new script. I will get to it in a minute.
 
-    return avg_flips , prob_s, std_flips, tts_99
+    return avg_flips, prob_s, std_flips, tts_99, avg_loops, std_loops, avg_tries, std_tries
 
 
 def main():
@@ -220,7 +244,7 @@ def main():
             walksat_result.append(res.get())
     pool.close()
     pool.join()
-    avg_flips, prob_s, std_flips, tts_99 = get_stats(walksat_result, max_flips)
+    avg_flips, prob_s, std_flips, tts_99, avg_loops, std_loops, avg_tries, std_tries = get_stats(walksat_result, max_flips)
 
     print("Average Number of Flips: ",avg_flips)
     print("Average Probability of Success: ",prob_s)
